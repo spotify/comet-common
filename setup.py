fe6109900753fa84ba50e2018cea949f366e4899
Benjamin Greschbach <bgre@spotify.com>
2019-01-16 13:25:20 +0100

    up comet-core version, upgrade marshmallow, and up own version to 1.0.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [r for r in requirements if not r.startswith('-e')]
 
 setuptools.setup(
     name="comet-common",
-    version="1.0.6",
+    version="1.0.8",
     url="https://github.com/spotify/comet-common",
 
     author="Spotify Platform Security",
