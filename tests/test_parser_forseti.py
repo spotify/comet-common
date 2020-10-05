80805cc214fee63ece27533d0cb37085efa0e669
Emma Filipsson <efilipsson@spotify.com>
2020-10-05 16:21:03 +0200

    test

--- tests/test_parser_forseti.py
+++ tests/test_parser_forseti.py
@@ -42,14 +42,13 @@ def test_ForsetiSchema_parsedok():
 def test_ForsetiSchema_fail():
     payload_ForsetiSchema = dict(_payload_ForsetiSchema)
     err = ForsetiSchema().validate(payload_ForsetiSchema)
-    assert "Field may not be null." in err.get("resource")
-    assert "Field may not be null." in err.get("resource_type")
-    assert "Forseti requires resource field" in err.get("resource")
+    assert "resource" in err
+    assert "resource_type" in err
 
     payload_ForsetiSchema["resource"] = list(SUPPORTED_RESOURCE.keys())[0]
     err = ForsetiSchema().validate(payload_ForsetiSchema)
-    assert "Field may not be null." in err.get("resource_type")
-    assert "Forseti requires resource_type field" in err.get("resource_type")
+    assert "resource_type" in err
+    assert "resource" not in err
 
     payload_ForsetiSchema["resource_type"] = SUPPORTED_RESOURCE_TYPES[0]
     err = ForsetiSchema().validate(payload_ForsetiSchema)
